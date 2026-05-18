4e0deea972a177e5fe549db765aadbe9bc020e7d
jurmed1798 <jurmedfaceit@gmail.com>
2026-05-18 16:50:04 +0900

    DailySpend week12

---
+++ app/src/main/java/com/example/dailyspend/others/AppTitle2.kt
@@ -0,0 +1,11 @@
+package com.example.dailyspend.others
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppTitle2(){
+    Text(
+        text="dailyspend2"
+    )
+}
